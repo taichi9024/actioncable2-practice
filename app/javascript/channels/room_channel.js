42c9a117e581a3a5d7b2296983694adb15603b81
taichi9024 <ebh48978@gmail.com>
2021-01-28 23:31:08 +0900

    rubocop

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,27 @@
+import consumer from "./consumer"
+
+const chatChannel = consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    return $("#messages").append(data["message"]);
+    // Called when there's incoming data on the websocket for this channel
+  },
+
+  speak: function(data) {
+    Message.create content: data['message']
+  });
+
+    $(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+      if (event.keyCode === 13) {
+        chatChannel.speak(event.target.value);
+        event.target.value = '';
+        return event.preventDefault();
+      }
+    });
